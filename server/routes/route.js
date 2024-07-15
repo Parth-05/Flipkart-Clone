eafc3a58aadec2f7aa92295dff1a49dccc776171
Parth-05 <parthmarathe5@gmail.com>
2024-07-14 22:52:31 -0700

    Added server code

---
+++ server/routes/route.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { userSignup, userLogin } from '../controllers/user-controller.js';
+import { getProducts, getProductById } from '../controllers/product-controller.js'
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Server is up and running!');
+});
+
+router.post('/signup', userSignup);
+
+router.post('/login', userLogin);
+
+router.get('/products', getProducts);
+
+router.get('/product/:id', getProductById);
+
+export default router;
