4310714a55abefe61aaa8b45bb03467eb90ae4f9
Parth-05 <parthmarathe5@gmail.com>
2024-07-14 23:49:00 -0700

    Updated code to catch errors

--- server/server.js
+++ server/server.js
@@ -26,10 +26,20 @@ const PASSWORD = process.env.DB_PASSWORD;
 
 const URL = process.env.MOGODB_URI
 
-Connection(URL);
+// Connection(URL);
 
-app.listen(PORT, () => {
-    console.log(`Server is running successfully on PORT: ${PORT}`);
+// app.listen(PORT, () => {
+//     console.log(`Server is running successfully on PORT: ${PORT}`);
+// });
+
+// DefaultData();
+
+Connection(URL).then(() => {
+    console.log('Database connected successfully');
+    app.listen(PORT, () => {
+        console.log(`Server is running successfully on PORT: ${PORT}`);
+        DefaultData();
+    });
+}).catch((error) => {
+    console.log(`Error connecting to database: ${error.message}`);
 });
-
-DefaultData();
